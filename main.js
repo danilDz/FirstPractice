b7929a4cc2355d2f207d813f1a6023736325e23f
Danil <example@gmail.com>
2020-12-25 19:40:37 +0200

    Third commit

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ let personalMovieDB={
     privat:false
 };
 
+let movie1,
+    movie2,
+    rate1,
+    rate2;
+movie1=prompt('Один из последних просмотренных фильмов.');
+rate1=prompt('На сколько вы его оцените?');
+movie2=prompt('Один из последних просмотренных фильмов.');
+rate2=prompt('На сколько вы его оцените?');
+
+personalMovieDB={
+    movies:{
+        movie1:rate1,
+        movie2:rate2
+    }
+};
+
+console.log(personalMovieDB);
