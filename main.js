7baf99bd08637d5b6e822e3f6e9a54dc88cde6dd
Danil <example@gmail.com>
2020-12-25 19:32:31 +0200

    Second commit

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ let numberOfFilms;
 
 numberOfFilms=prompt('Сколько фильмов вы уже посмотрели?');
 
+let personalMovieDB={
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
